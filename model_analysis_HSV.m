721d41a0f16e77134722e5deaf386051a0e5548d
justaneater <96414401+justaneater@users.noreply.github.com>
2022-12-16 22:55:12 +0800

    Update model_analysis_HSV.m

--- model_analysis_HSV.m
+++ model_analysis_HSV.m
@@ -3,7 +3,7 @@ clc,clear,close all
 [a,b]=meshgrid(-1:0.01:1,1:-0.01:-1);
 h=atan2(b,a)/(2*pi); h(h<0)=h(h<0)+1;
 s=sqrt(a.^2+b.^2);
-%% gamut checking
+% gamut checking
 for V=0:100
     hsv=cat(3,h,s,V*ones(201,201)/100);
     rgb=hsv2rgb(hsv);
@@ -11,13 +11,14 @@ for V=0:100
     mask=R>1|R<0|G>1|G<0|B>1|B<0;
     R(mask)=0; G(mask)=0; B(mask)=0;
     RGB=cat(3,R,G,B);
-%     imshow(RGB,'Border','tight')
+    imshow(RGB,'Border','tight')
     HSV{V+1}=RGB;
 end
-figure, montage(HSV,'Indices',2:101,'Size',[10 10],...
-                'BorderSize',1,'BackgroundColor','w')
+montage(HSV,'Indices',2:101,'Size',[10 10],...
+        'BorderSize',1,'BackgroundColor','w')
 
-%% grayspace threshold
+% grayspace threshold
+figure
 for V=0:100
     hsv=cat(3,h,s,V*ones(201,201)/100);
     rgb=hsv2rgb(hsv);
@@ -28,8 +29,8 @@ for V=0:100
     mask=abs(R-G)<thres & abs(R-B)<thres & abs(G-B)<thres;
     R(~mask)=0; G(~mask)=0; B(~mask)=0;
     RGB=cat(3,R,G,B);
-%     imshow(RGB,'Border','tight')
+    imshow(RGB,'Border','tight')
     HSV{V+1}=RGB;
 end
-figure, montage(HSV,'Indices',2:101,'Size',[10 10],...
-                'BorderSize',1,'BackgroundColor','w')
+montage(HSV,'Indices',2:101,'Size',[10 10],...
+        'BorderSize',1,'BackgroundColor','w')
